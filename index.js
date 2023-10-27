4bfeacf5fc2a95358cd7283990c3aa93063872e6
Nicolas Tomas <nicolastomas328@gmail.com>
2023-10-26 22:13:53 -0300

    ultimos cambios

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 // Elementos del DOM
 const idiomaButton = document.querySelector(".idioma");
-const diaNocheButton = document.querySelector(".diaNoche");
+const diaNocheButton = document.querySelector(".form-check-input");
 
 const elementsToTranslate = {
     spanishB: document.querySelector(".spanishB"),
+    spanish: document.querySelector(".spanish"),
     english: document.querySelector(".english"),
     descripcionTitle: document.querySelector(".descripcion h1"),
     descripcion: document.querySelector(".descripcion p"),
@@ -29,12 +30,17 @@ function applyLanguagePreferences() {
     elementsToTranslate.english.classList.toggle("text-light", isEnglish);
     elementsToTranslate.descripcionTitle.innerHTML = isEnglish ? "Hello!! My name is Nicolas" : "Hola!! Soy Nicolas";
     elementsToTranslate.descripcion.innerHTML = isEnglish ?
-        "I am a frontend developer with experience in React. I'm good at CSS and HTML, as well as JavaScript. I love learning new things and experimenting with new technologies. I have a good eye for details and always put my all into my work." :
-        "Soy desarrollador frontend con experiencia en React. Soy bueno en CSS y HTML, así como en JavaScript. Me encanta aprender cosas nuevas y experimentar con nuevas tecnologías. Tengo buen ojo para los detalles y siempre pongo todo de mí en mi trabajo.";
+        "I am a frontend developer. I'm good at CSS and HTML, as well as JavaScript. I love learning new things and experimenting with new technologies. I have a good eye for detail and always put my all into my work." :
+        "Soy desarrollador frontend. Soy bueno en CSS y HTML, así como en JavaScript. Me encanta aprender cosas nuevas y experimentar con nuevas tecnologías. Tengo buen ojo para los detalles y siempre pongo todo de mí en mi trabajo.";
     elementsToTranslate.contacto.innerHTML = isEnglish ? "Contact" : "Contacto";
     elementsToTranslate.informacion.innerHTML = isEnglish ? "information" : "Información";
     elementsToTranslate.desarrollador.innerHTML = isEnglish ? "Software Developer" : "Desarrollador de Sofware";
-    elementsToTranslate.globo.innerHTML = isEnglish ? "Talk to mi clon!!" : "Habla con mi clon!!";
+    elementsToTranslate.globo.innerHTML = isEnglish ? "Talk to mi clon!!" : "Hablá con mi clon!!";
+    elementsToTranslate.edad.innerHTML = isEnglish ? "Years Old" : "Años";
+    elementsToTranslate.edad.innerHTML = isEnglish ? "Years Old" : "Años";
+    elementsToTranslate.ingles.innerHTML = isEnglish ? "English Level" : "Nivel de ingles";
+    elementsToTranslate.proyectos.innerHTML = isEnglish ? "Projects" : "Proyectos";
+    elementsToTranslate.spanish.innerHTML = isEnglish ? "EN" : "ES";
 }
 
 // Función para aplicar preferencias de tema
